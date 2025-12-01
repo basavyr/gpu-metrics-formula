2f6dc7bc69131e3106e09c554c4000e5f4bcaf10
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-01 09:08:20 +0200

    compute gpu score

--- src/gpu.py
+++ src/gpu.py
@@ -1,7 +1,92 @@
+from dataclasses import dataclass
+import logging
+
+from math import sqrt, log, pow
+
+from data import GPU_METRICS_DEFAULT_WEIGHTS, GPU_REFERENCE_METRICS
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(message)s',
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+
+@dataclass
+class GPUMetric:
+    name: str
+    vram: float
+    bandwidth: float
+    tdp: float
+    cost_ron: float
+
+
+@dataclass
+class GPUDATA:
+    RTX_5070TI = GPUMetric(name="RTX_5070TI", vram=16,
+                           bandwidth=896, tdp=300, cost_ron=4500)
+    RX_9070XT = GPUMetric(name="RX_9070XT", vram=16,
+                          bandwidth=650, tdp=304, cost_ron=3500)
+    RX_7900XTX = GPUMetric(name="RX_7900XTX", vram=24,
+                           bandwidth=960, tdp=400, cost_ron=5000)
+
+
+class GPUMetricTransform:
+    @staticmethod
+    def f_M_j(gpu_vram: float):
+        return log(1.0 + gpu_vram)
+
+    @staticmethod
+    def f_B_j(gpu_bandwidth: float):
+        return sqrt(gpu_bandwidth)
+
+    @staticmethod
+    def f_P_j(gpu_tdp: float):
+        return log(1.0 + (1.0 / gpu_tdp))
+
+    @staticmethod
+    def f_C_j(gpu_cost: float):
+        return log(1.0 + (1.0 / gpu_cost))
+
+    @staticmethod
+    def u_M_j(gpu_metrics: GPUMetric):
+        f_M_j = GPUMetricTransform.f_M_j(gpu_metrics.vram)
+        f_M_ref = GPUMetricTransform.f_M_j(GPU_REFERENCE_METRICS['vram'])
+        return f_M_j/f_M_ref
+
+    @staticmethod
+    def u_B_j(gpu_metrics: GPUMetric):
+        f_B_j = GPUMetricTransform.f_B_j(gpu_metrics.bandwidth)
+        f_B_ref = GPUMetricTransform.f_B_j(GPU_REFERENCE_METRICS['bandwidth'])
+        return f_B_j/f_B_ref
+
+    @staticmethod
+    def u_P_j(gpu_metrics: GPUMetric):
+        f_P_j = GPUMetricTransform.f_P_j(gpu_metrics.tdp)
+        f_P_ref = GPUMetricTransform.f_P_j(GPU_REFERENCE_METRICS['tdp'])
+        return f_P_j/f_P_ref
+
+    @staticmethod
+    def u_C_j(gpu_metrics: GPUMetric):
+        f_C_j = GPUMetricTransform.f_C_j(gpu_metrics.cost_ron)
+        f_C_ref = GPUMetricTransform.f_C_j(GPU_REFERENCE_METRICS['cost_ron'])
+        return f_C_j/f_C_ref
+
+
+def compute_gpu_score(gpu: GPUMetric, rho: float = 1, metric_weights: dict = GPU_METRICS_DEFAULT_WEIGHTS) -> float:
+    s_vram = metric_weights["vram"]*pow(GPUMetricTransform.u_M_j(gpu), rho)
+    s_bandwidth = metric_weights["bandwidth"] * \
+        pow(GPUMetricTransform.u_B_j(gpu), rho)
+    s_tdp = metric_weights["tdp"]*pow(GPUMetricTransform.u_P_j(gpu), rho)
+    s_cost = metric_weights["cost_ron"]*pow(GPUMetricTransform.u_C_j(gpu), rho)
+    s = round(pow(s_vram+s_bandwidth+s_tdp+s_cost, 1.0/rho), 3)
+
+    logger.info(f'GPU: {gpu.name} | Score: {s}')
+    return s
 
 
 def main():
-    return
+    compute_gpu_score(GPUDATA.RX_9070XT)
+    compute_gpu_score(GPUDATA.RTX_5070TI)
+    compute_gpu_score(GPUDATA.RX_7900XTX)
 
 
 if __name__ == "__main__":
