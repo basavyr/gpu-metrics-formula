966a6f768ace501e300893feadcff8a79f841a51
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-01 09:08:06 +0200

    add default reference values and weights

---
+++ src/data.py
@@ -0,0 +1,13 @@
+GPU_REFERENCE_METRICS = {
+    "vram": 32.0,
+    "bandwidth": 1000.0,
+    "tdp": 400.0,
+    "cost_ron": 3500,
+}
+
+GPU_METRICS_DEFAULT_WEIGHTS = {
+    "vram": 0.25,
+    "bandwidth": 0.35,
+    "tdp": 0.2,
+    "cost_ron": 0.2,
+}
